7f56c56046942b8ac781f4cb017ea5580d774071
playduck <robin.prillwitz@gmail.com>
2021-08-04 17:17:05 +0200

    added channel support

--- inc/pipeline.h
+++ inc/pipeline.h
@@ -1,4 +1,4 @@
-// #ifndef PIPELINE_H
+#ifndef PIPELINE_H
 #define PIPELINE_H
 
 #include <stdint.h>
@@ -7,6 +7,11 @@
 #include "iir.h"
 #include "gate.h"
 
+typedef enum {
+	CHANNEL_ONE,
+	CHANNEL_TWO,
+} channel_t;
+
 void initilize(int);
-void process_sample(int16_t *, int16_t *);
-// #endif
+void process_sample(channel_t, int16_t *, int16_t *);
+#endif
--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ uint16_t process(int16_t* s, uint32_t len, uint16_t samplerate) {
     initilize(samplerate);
 
     for(uint32_t i = 0; i < len; i++)   {
-        process_sample(
+        process_sample(CHANNEL_ONE,
             s +     2*i,
             s + 1 + 2*i
         );
