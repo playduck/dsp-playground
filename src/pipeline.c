3bd600ca4dc1d688cf013314c88d96b257c4e507
robin <robin.prillwitz@gmail.com>
2022-02-19 10:24:18 +0100

    added lowcut

--- src/pipeline.c
+++ src/pipeline.c
@@ -17,16 +17,19 @@ int16_t float_to_fixed(sample_t s)
 
 void initilize(uint16_t samplerate)
 {
-    vol[0] = generate_gain(-3.0f);
-    vol[1] = generate_gain(-3.0f);
-
+    // crossover lowpass
     filters[0] = generate_biquad(lowpass, 2000, 0.54119610f, 0, samplerate);
     filters[1] = generate_biquad(lowpass, 2000, 1.3065630f, 0, samplerate);
-
+    // crossover highpass
     filters[2] = generate_biquad(highpass, 2000, 0.54119610f, 0, samplerate);
     filters[3] = generate_biquad(highpass, 2000, 1.3065630f, 0, samplerate);
 
+    // lowcut
+    filters[4] = generate_biquad(highpass, 40, 0.54119610f, 0, samplerate);
+    filters[5] = generate_biquad(highpass, 40, 1.3065630f, 0, samplerate);
+
     delay[0] = generate_delay_line(40, 0.2, samplerate);
+    vb[0] = generate_virtual_bass(100, 220, -34.0, samplerate);
 
     gate[0] = generate_gate(-40, 0.7, 0.8, 0.00001, 0, samplerate);
     gate[1] = generate_gate(-40, 0.7, 0.8, 0.00001, 0, samplerate);
@@ -34,7 +37,8 @@ void initilize(uint16_t samplerate)
     compressor[0] = generate_compressor(-8, 4, 3, 0.00001, 0.0001, -1, samplerate);
     compressor[1] = generate_compressor(-8, 4, 3, 0.00001, 0.0001, -1, samplerate);
 
-    vb[0] = generate_virtual_bass(100, 220, -34.0, samplerate);
+    vol[0] = generate_gain(-3.0f);
+    vol[1] = generate_gain(-3.0f);
 }
 
 void process_sample(int16_t *i16pSample1, int16_t *i16pSample2)
@@ -43,18 +47,25 @@ void process_sample(int16_t *i16pSample1, int16_t *i16pSample2)
     sample2 = fixed_to_float(i16pSample2);
     float mono = (sample1 + sample2) / 2.0f;
 
+    // lowcut
+    biquad_filter(&mono, filters + 4);
+    biquad_filter(&mono, filters + 5);
+
     float lf = mono;
     float hf = mono;
 
+    // crossover
     biquad_filter(&lf, filters + 0);
     biquad_filter(&lf, filters + 1);
 
     biquad_filter(&hf, filters + 2);
     biquad_filter(&hf, filters + 3);
 
+    // fx
     virtual_bass(&lf, vb);
     delay_line(&hf, delay);
 
+    // dynamics
     dynamic_compressor(&lf, compressor + 0);
     dynamic_compressor(&hf, compressor + 1);
 
